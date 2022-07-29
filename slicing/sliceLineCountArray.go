51f81b68e5031db2fcbd0c6a7302eed742fa02d4
robinhz <robin.heinz@posteo.de>
2022-07-29 16:03:15 +0200

    Fix slice indexing bug

--- slicing/sliceLineCountArray.go
+++ slicing/sliceLineCountArray.go
@@ -3,8 +3,8 @@ package slicing
 import "locvis/entities"
 
 func GetTopTenSlice(lineCounts []entities.LineCount) []entities.LineCount {
-	if len(lineCounts) < 10 {
+	if len(lineCounts) <= 10 {
 		return lineCounts
 	}
-	return lineCounts[0:9]
+	return lineCounts[0:10]
 }
