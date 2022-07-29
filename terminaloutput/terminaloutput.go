c5473a3b1ffd45282fb32f10e83eb29f0f066320
robinhz <robin.heinz@posteo.de>
2022-07-29 17:07:46 +0200

    Add table to terminal output

--- terminaloutput/terminaloutput.go
+++ terminaloutput/terminaloutput.go
@@ -1,12 +1,21 @@
 package terminaloutput
 
 import (
-	"fmt"
+	"github.com/jedib0t/go-pretty/v6/table"
 	"locvis/entities"
+	"os"
+	"path"
 )
 
 func PrintLineCount(lineCounts []entities.LineCount) {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"LoC", "Filename", "Path"})
+
 	for _, lineCount := range lineCounts {
-		fmt.Println(lineCount.LineCount, " ", lineCount.Path)
+		var filename string = path.Base(lineCount.Path)
+		t.AppendRow(table.Row{lineCount.LineCount, filename, lineCount.Path})
+		//fmt.Println(lineCount.LineCount, " ", filename, " ", lineCount.Path)
 	}
+	t.Render()
 }
